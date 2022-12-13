6e0e3c5fdf0385aa821ea34678aee0512e014ff8
Aphans <rapperanqeel@hotmail.com>
2022-12-13 13:40:35 +0100

    Delete button to show calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,5 @@
 <template>
-    <button @click="show=!show" type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Mes</button>
-    <div id="calendar">
+      <div id="calendar">
     <div class="calendar-header">
       <div class="flex justify-between items-center px-2">
         <button @click="prev">
@@ -8,7 +7,6 @@
             <path d="M15.41 7.41L14 6L8 12L14 18L15.41 16.59L11.83 13H20V11H11.83L15.41 7.41Z" fill="currentColor"/>
           </svg>
         </button>
-        <h3 class="text-xl text-center">{{currentMonthName}}, {{currentYear}}</h3>
         <button @click="next">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z" fill="currentColor"/>
@@ -16,14 +14,13 @@
         </button>
       </div>
     </div>
-    <section v-if="show">
+      <h3 class="text-xl text-center">{{currentMonthName}}, {{currentYear}}</h3>
         <div class="calendar-body grid grid-cols-7 gap-1 px-2">
-      <div v-for="day in daysWeek" :key="day" class="text-center font-bold">{{day}}</div>
-      <div :class="todayDate(date)" class="w-40 " v-for="(date,idx) in lastDateOfMonth" v-bind:key="idx">{{date}}
+      <div v-for="day in daysWeek" :key="day" class=" font-bold">{{day}}</div>
+      <div :class="todayDate(date)" class="w-60" v-for="(date,idx) in lastDateOfMonth" v-bind:key="idx">{{date}}
     </div>
     </div>
-    </section>
-</div>
+    </div>
 </template>
 <script setup>
 import {ref,computed} from "vue";
@@ -58,8 +55,10 @@ const currentMonthName = computed(()=>{
    return new Date(currentYear.value,currentMonthInNumber.value).toLocaleString("default",{month:"long"})});
 const lastDateOfMonth = computed(()=>{
     return new Date(currentYear.value,currentMonthInNumber.value+1,0).getDate()});
-const firstDay = computed(()=>{
-    return new Date(currentYear.value,currentMonthInNumber.value+1,0).getDate()});
+    const firstDay = computed(() => {
+  return new Date(currentYear.value, currentMonthInNumber.value, 1).getDay();
+});
+
 </script>
 <style scoped>
 </style>
