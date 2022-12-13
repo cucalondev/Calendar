cfb9417ee9cb8f5123168d0ef8183497ec33baf1
Aphans <rapperanqeel@hotmail.com>
2022-12-13 12:40:59 +0100

    Rework template, responsive calendar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-  <Navbar/>
   <Calendar/>
 </div>
 </template>
 <script setup>
 import Calendar from "@/components/Calendar.vue";
-import Navbar from "@/components/Navbar.vue";
 </script>
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,28 +1,27 @@
 <template>
-<div class="container">
     <button @click="show=!show" type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Mes</button>
-    <section v-if="show">
-    <h1>{{currentMonthName}} {{currentYear}}</h1>
-    <div class="days flex">
-    <p class="w-40" v-for="(day,index) in daysWeek" v-bind:key="index">
-    {{day}}
-    </p>
+    <div id="calendar">
+    <div class="calendar-header">
+      <div class="flex justify-between items-center px-2">
+        <button @click="prev">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.41 7.41L14 6L8 12L14 18L15.41 16.59L11.83 13H20V11H11.83L15.41 7.41Z" fill="currentColor"/>
+          </svg>
+        </button>
+        <h3 class="text-xl text-center">{{currentMonthName}}, {{currentYear}}</h3>
+        <button @click="next">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z" fill="currentColor"/>
+          </svg>
+        </button>
+      </div>
     </div>
-    </section>
     <section v-if="show">
-    <table class="dates flex flex-wrap">
-        <tr class="w-40" v-for="day in firstDay" v-bind:key="day">{{}}</tr>
-        <tr :class="todayDate(date)" class="w-40" v-for="(date,idx) in lastDateOfMonth" v-bind:key="idx">{{date}}
-        </tr>
-    </table>
-    </section>
-    <section class="flex justify-between">
-        <button @click="prev" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Prev
-        </button>
-        <button @click="next" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Next
-        </button>
+        <div class="calendar-body grid grid-cols-7 gap-1 px-2">
+      <div v-for="day in daysWeek" :key="day" class="text-center font-bold">{{day}}</div>
+      <div :class="todayDate(date)" class="w-40 " v-for="(date,idx) in lastDateOfMonth" v-bind:key="idx">{{date}}
+    </div>
+    </div>
     </section>
 </div>
 </template>
